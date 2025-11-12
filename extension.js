7748aba242c4167c806b9527b8c486ca60fe1923
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-12 18:24:23 +0600

    Alhamdulillah, stable

--- extension.js
+++ extension.js
@@ -24,6 +24,9 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 import { setLogging, setLogFn, journal } from './utils.js'
 
+const AppSystem = global.get_app_system();
+const DECODER = new TextDecoder();
+
 export default class MyExtension extends Extension {
 
     constructor(metadata) {
@@ -67,14 +70,14 @@ export default class MyExtension extends Extension {
         // Name of the service emitting the D-Bus signal
         let serviceName = null; // Since sender is (null destination)
 
-        // D-Bus path emitting the signal
-        let objectPath = "/org/gtk/gio/DesktopAppInfo";
-
         // Interface emitting the signal
         let interfaceName = "org.gtk.gio.DesktopAppInfo";
 
         // Name of the signal
         let signalName = "Launched";
+
+        // D-Bus path emitting the signal
+        let objectPath = "/org/gtk/gio/DesktopAppInfo";
 
         // Connect to the signal
         this._id = this._connection.signal_subscribe(
@@ -100,38 +103,80 @@ export default class MyExtension extends Extension {
     }
 
     _handleSignal(connection, senderName, objectPath, interfaceName, signalName, parameters) {
-        // Parse and handle the signal data
-        let [filePathBytes, _, processId, urls] = parameters.deep_unpack();
+        journal(`parameters : ${parameters.print(true)}`);
+        journal(`parameters Type : ${parameters.get_type_string()}`);
 
-        let pid = processId;
+        let [desktopFileBytes, appId, pid, uris, metadata] = parameters.deepUnpack();
 
-        let decoder = new TextDecoder();
-        let desktopFilePath = decoder.decode(filePathBytes);
-        // Remove null character from the end of the string if present
-        desktopFilePath = desktopFilePath.replace(/\0/g, '');
-        // Assuming the parameters are in the given order
+        // Convert the byte array to string
+        // let desktopFile = imports.byteArray.toString(desktopFileBytes);
+        let desktopFile = DECODER.decode(desktopFileBytes);
 
-        // let filePath = GLib.bytes_to_string(filePathBytes);
-        // encoded_path = GLib.filename_to_utf8(desktop_file_path)[0];
+        let app = Gio.DesktopAppInfo.new_from_filename(desktopFile);
 
-        let app = Gio.DesktopAppInfo.new_from_filename(desktopFilePath);
+        let app_Id = app.get_id();
 
-        let app_id = app.get_id();
+        journal(`app_Id : ${app_Id}`);
 
-        journal(`before data`);
+        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
+
+            let shell_apps = AppSystem.lookup_app(app_Id);
+
+            journal(`shell_apps : ${shell_apps}`);
+
+            let windows_array = [];
+
+            shell_apps.get_windows().forEach(function (w) {
+                // console.log("window id : " + w.get_id());
+                windows_array.push(w.get_id());
+            })
+
+            let pids_array = shell_apps.get_pids();
+
+
+
+        // journal(`Desktop File:", ${desktopFile}`);
+        // journal(`App ID:", ${app_Id}`);
+        // journal(`PID:", ${pid}`);
+        // journal(`URIs:", ${uris.join(', ')}`);
+
+        // // metadata is a JS object mapping string → Variant
+        // for (let [key, value] of Object.entries(metadata)) {
+        //     journal(`${key}: ${value.deepUnpack()}`);
+        // }
+
+        // // Parse and handle the signal data
+        // // let [desktop, appid, pid, uris, extras] = params.deep_unpack();
+        // let [filePathBytes, _, processId, urls] = parameters.deep_unpack();
+
+        // let pid = processId;
+
+        // // Remove null character from the end of the string if present
+        // desktopFilePath = desktopFilePath.replace(/\0/g, '');
+        // // Assuming the parameters are in the given order
+
+        // // let filePath = GLib.bytes_to_string(filePathBytes);
+        // // encoded_path = GLib.filename_to_utf8(desktop_file_path)[0];
+
+        // journal(`before data`);
 
         // Create an object with the parsed data
         let data = {
-            "app_id": app_id,
-            "process_id": pid,
-            "urls": urls
+            app_Id: app_Id,
+            pid: pids_array,
+            windows: windows_array,
+            uris: uris
         };
 
         // Convert the object to JSON string
         let json_data = JSON.stringify(data);
 
-        // Append the JSON data to the file
-        this._appendToFile(json_data);
+        journal(`json_data: ${json_data}`);
+
+        // // Append the JSON data to the file
+        // this._appendToFile(json_data);
+            return GLib.SOURCE_REMOVE; // important to avoid repeated execution
+        });
     }
 
     _appendToFile(data) {
@@ -142,7 +187,7 @@ export default class MyExtension extends Extension {
         let outputStream = this._file.append_to(Gio.FileCreateFlags.NONE, null);
 
         // Write the data to the file
-        outputStream.write(data, null);
+        outputStream.write(data + "\n", null);
         outputStream.close(null);
     }
 }
